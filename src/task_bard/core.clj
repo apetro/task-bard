2b8837b431943000a20075feb3e4fb98b68a65ce
Andrew Petro <andrew.petro@hey.com>
2022-11-02 21:56:01 -0500

    Prompt for a task, then hound the user until the task is complete.

--- src/task_bard/core.clj
+++ src/task_bard/core.clj
@@ -1,7 +1,18 @@
 (ns task-bard.core
   (:gen-class))
+(use 'pretty.cli.prompt)
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (while true
+    (let
+      [task
+        (input "What needs doing?" (fn [x] (if (= "" x) "Sorry?")))]
+      (println)
+      (println "Okay. Then do " task)
+      (while (not (confirm (str "Is " task " done yet"))))
+      (println)
+      (println "Congratulations on doing " task)
+      (println))))
